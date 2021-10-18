82d51e5c42b8cdfe5e20f675f75ca30f2a1d5d0d
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2021-10-19 00:14:19 +0200

    Ownable & Pausable

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # EVM Cross Deployer
 
 ## Test Deployments
-- Rinkeby: https://rinkeby.etherscan.io/address/0x8569aD545A731B3b95C12B463F0EcB0B7ECe2348
+- Rinkeby: `[]()`
+- Ropsten: `[]()`
+- Kovan: `[]()`
+- Goerli: `[]()`
--- contracts/Create2Deployer.sol
+++ contracts/Create2Deployer.sol
@@ -1,13 +1,43 @@
 // SPDX-License-Identifier: MIT
-// See: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/mocks/Create2Impl.sol
+// Further information: https://eips.ethereum.org/EIPS/eip-1014
 
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Create2.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC1820Implementer.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract Create2Deployer {
-    function deploy(
+
+/**
+ * @title CREATE2 Deployer Smart Contract
+ * @author Pascal Marco Caversaccio, pascal.caversaccio@hotmail.ch
+ * @dev Helper smart contract to make usage of the `CREATE2` EVM opcode easier and safer.
+ * `CREATE2` can be used to compute in advance the address where a smart
+ * contract will be deployed, which allows for interesting new mechanisms known
+ * as 'counterfactual interactions'.
+ *
+ * See the https://eips.ethereum.org/EIPS/eip-1014#motivation[EIP] for more
+ * information.
+ */
+
+contract Create2Deployer is Ownable, Pausable {
+
+    /**
+     * @dev Deploys a contract using `CREATE2`. The address where the contract
+     * will be deployed can be known in advance via {computeAddress}.
+     *
+     * The bytecode for a contract can be obtained from Solidity with
+     * `type(contractName).creationCode`.
+     *
+     * Requirements:
+     *
+     * - `bytecode` must not be empty.
+     * - `salt` must have not been used for `bytecode` already.
+     * - the factory must have a balance of at least `amount`.
+     * - if `amount` is non-zero, `bytecode` must have a `payable` constructor.
+     */
+    function deploy whenNotPaused() (
         uint256 value,
         bytes32 salt,
         bytes memory code
@@ -15,14 +45,26 @@ contract Create2Deployer {
         Create2.deploy(value, salt, code);
     }
 
-    function deployERC1820Implementer(uint256 value, bytes32 salt) public {
+    /**
+     * @dev Deployment of the {ERC1820Implementer}.
+     * See https://eips.ethereum.org/EIPS/eip-1820
+     */
+    function deployERC1820Implementer(uint256 value, bytes32 salt) public whenNotPaused() {
         Create2.deploy(value, salt, type(ERC1820Implementer).creationCode);
     }
 
+    /**
+     * @dev Returns the address where a contract will be stored if deployed via {deploy}. Any change in the
+     * `bytecodeHash` or `salt` will result in a new destination address.
+     */
     function computeAddress(bytes32 salt, bytes32 codeHash) public view returns (address) {
         return Create2.computeAddress(salt, codeHash);
     }
 
+    /**
+     * @dev Returns the address where a contract will be stored if deployed via {deploy} from a contract located at
+     * `deployer`. If `deployer` is this contract's address, returns the same value as {computeAddress}.
+     */
     function computeAddressWithDeployer(
         bytes32 salt,
         bytes32 codeHash,
@@ -31,5 +73,31 @@ contract Create2Deployer {
         return Create2.computeAddress(salt, codeHash, deployer);
     }
 
+    /**
+    * @dev Contract can receive ether. The only way to transfer this ether however is to call the `killDeployer` function.
+    */
     receive() external payable {}
+
+    /**
+     * @dev Triggers stopped state.
+     * Requirements: The contract must not be paused.
+     */
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Returns to normal state.
+     * Requirements: The contract must be paused.
+     */
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    /// @dev Destroys the Create2Deployer contract and transfers all ether to a pre-defined payout address.
+    function killDeployer(address payable payoutAddress) public onlyOwner() {
+        payoutAddress.transfer(address(this).balance);
+        selfdestruct(payoutAddress);
+    }
+
 }
