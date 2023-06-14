b1848c7871231895f808955442cb51f318b426dc
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-06-14 14:51:04 +0200

    🔁 Bump deps
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,7 +34,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.3.10",
+    version: "1.3.11",
     compilerSource: "binary",
     settings: {
       isSystem: false,
