d8285a8998d1bc6f2e4db1b2ea037e57a42322f9
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-10-21 12:15:54 +0200

    ♻️  Update `arbitrumSepolia` Explorer Link
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -639,8 +639,8 @@ const config: HardhatUserConfig = {
         network: "arbitrumSepolia",
         chainId: 421614,
         urls: {
-          apiURL: "https://sepolia-explorer.arbitrum.io/api",
-          browserURL: "https://sepolia-explorer.arbitrum.io",
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io",
         },
       },
       {
