146752cd9a75c80139f3036565f4d1e9b9c50436
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-10-28 12:13:13 +0200

    ♻️ Update `zksolc` Version
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,7 +34,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.3.14",
+    version: "1.3.15",
     compilerSource: "binary",
     settings: {
       isSystem: false,
