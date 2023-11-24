53a1062aa4df25dc0b632f1f3f234051f5ba2287
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-11-24 14:32:11 +0100

    ♻️  Refactor Deployment Scripts
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- deploy/deploy-zksync.ts
+++ deploy/deploy-zksync.ts
@@ -5,6 +5,10 @@ import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
 const PRIVATE_KEY: string = process.env.PRIVATE_KEY_ZKS || "";
 
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export default async function main(hre: HardhatRuntimeEnvironment) {
   const wallet = new Wallet(PRIVATE_KEY);
   const deployer = new Deployer(hre, wallet);
@@ -13,6 +17,13 @@ export default async function main(hre: HardhatRuntimeEnvironment) {
   const create2Deployer = await deployer.deploy(artifact);
 
   await create2Deployer.deployed();
+  const create2DeployerAddress = create2Deployer.address;
 
-  console.log("Create2Deployer deployed to:", create2Deployer.address);
+  console.log("Create2Deployer deployed to:", create2DeployerAddress);
+
+  await delay(30000); // Wait for 30 seconds before verifying the contract
+
+  await hre.run("verify:verify", {
+    address: create2DeployerAddress,
+  });
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,22 @@
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 async function main() {
   const create2Deployer = await ethers.deployContract("Create2Deployer");
 
   await create2Deployer.deployed();
+  const create2DeployerAddress = create2Deployer.address;
 
-  console.log("Create2Deployer deployed to:", create2Deployer.address);
+  console.log("Create2Deployer deployed to:", create2DeployerAddress);
+
+  await delay(30000); // Wait for 30 seconds before verifying the contract
+
+  await hre.run("verify:verify", {
+    address: create2DeployerAddress,
+  });
 }
 
 main().catch((error) => {
