4041077c39c4f37aef0f3aea13b817226427d1f6
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-02-15 12:43:14 +0100

    ♻️ Use `eslint` Flat Configuration File `eslint.config.js`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -57,7 +57,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.3.22",
+    version: "1.3.23",
     compilerSource: "binary",
     settings: {
       isSystem: false,
