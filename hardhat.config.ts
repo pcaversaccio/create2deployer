e49ecf35705b1de04803e4b24df89e7a82919c48
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-06-30 15:01:02 +0200

    🔁 Bump `@eslint/js` to Version `9.6.0`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -59,7 +59,7 @@ const config: HardhatUserConfig = {
     },
   },
   zksolc: {
-    version: "1.5.0",
+    version: "1.5.1",
     compilerSource: "binary",
     settings: {
       enableEraVMExtensions: false,
